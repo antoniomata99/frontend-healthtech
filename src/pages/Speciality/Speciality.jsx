314d949129b3d7a08d1b2d13f17d5fd307b6e928
antoniomata99 <antoniomata99@hotmail.com>
2021-10-29 18:59:16 -0500

    Speciality works

--- src/pages/Speciality/Speciality.jsx
+++ src/pages/Speciality/Speciality.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import { Form, DataTable, DataItem, Message, DropDown, BackArrow } from '../../components'
+import { Form, DataTable, DataItem, Message, DropDown, BackArrow, Button} from '../../components'
 import './Speciality.scss'
+import { RiEdit2Line, RiDeleteBin6Line, RiEyeFill } from 'react-icons/ri'
 
 const titles = ['ID', 'Nombre', 'Descripción', 'Estado']
 const data = ['001', 'Urología', 'Profesional de Urología', 'Activo']
@@ -10,20 +11,23 @@ const activeElements = [
     {id: 2, value: true},
 ]
 
-const Speciality = () =>{
+
+
+const Speciality = ({view=false}) =>{
+
+    const [specialities, setSpecialities] = React.useState([])
+
     React.useEffect(() => {
         getData()
     }, [])
     
     const getData = async () => {
-
         const data = await fetch('https://healt-tech-back.herokuapp.com/api/especialidad/?format=json', {
             method: 'GET',
             mode: 'cors'
         })
-        
-        const jsonxd = await data.json()
-        console.log(jsonxd)
+        const specialities = await data.json()
+        setSpecialities(specialities)
     }
     return(
     <section className="Speciality">
@@ -35,8 +39,42 @@ const Speciality = () =>{
             </div>
         </Form>
         <div className='Schedule-Table'>
-            <DataTable titles={titles}>{data.length > 0 ? <DataItem data={data} /> : <Message text='No hay información disponible' />} </DataTable>
+        <div className='Table'>
+          <div className='Tableheader'>
+            <h2 className='TableTitle'>ID</h2>
+            <h2 className='TableTitle'>Nombre</h2>
+            <h2 className='TableTitle'>Descripción</h2>
+            <h2 className='TableTitle'>Estado</h2>
+            <h2 className='TableTitle'>Actions</h2>
+          </div>
+          <div className='TableContent'>
+            {specialities.map((item) => (
+              <div className='TableItem'>
+                <div className='TableData'>{item.id_especialidad}</div>
+                <div className='TableData'>{item.nombre}</div>
+                <div className='TableData'>{item.descripcion}</div>
+                <div className='TableData'>{item.estado}</div>
+                <div className='TableData'>
+                  <div className='DataItemButtons'>
+                    <Button modifier='edit'>
+                      <RiEdit2Line />
+                    </Button>
+                    <Button modifier='delete'>
+                      <RiDeleteBin6Line />
+                    </Button>
+                    {view && (
+                      <Button modifier='view'>
+                        <RiEyeFill />
+                      </Button>
+                    )}
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
+        {/* <DataTable titles={titles}>{datas.length > 0 ? datas.map((data, index) => <DataItem data={data} />) : null}</DataTable> */}
+      </div>
         <BackArrow link='/doctor' />
     </section>
     )
